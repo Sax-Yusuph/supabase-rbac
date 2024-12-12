285ea88338ef9f95a700ccf58d27706e07a32890
sax-yusuph <yusuphshamsondeen@gmail.com>
2024-12-12 19:39:53 +0100

    modifications

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,10 +1,10 @@
 -- Insert values into app_role table
-INSERT INTO public.app_role (name) VALUES
-    ('admin'),
-    ('member');
+-- INSERT INTO public.app_role (name) VALUES
+--     ('admin'),
+--     ('member');
 
 
-    -- Insert values into app_permission table
-    INSERT INTO public.app_permission (name) VALUES
-        ('workspace.read'),
-        ('workspace.delete');
+--     -- Insert values into app_permission table
+--     INSERT INTO public.app_permission (role, name) VALUES
+--         ('admin','workspace.read'),
+--         ('admin','workspace.delete');
